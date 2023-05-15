68b1a24f18da365f9e8d6a7f297997d5a9d7775a
TotoB12 <totobeliard@outlook.com>
2023-05-15 14:20:52 -0700

    ..

--- emlid_read_pc/emlid_read_pc.py
+++ emlid_read_pc/emlid_read_pc.py
@@ -1,35 +1,10 @@
-# import serial
-
-# def read_nmea_messages(port, baudrate):
-#     with serial.Serial(port, baudrate, timeout=1) as ser:
-#         while True:
-#             line = ser.readline().decode('ascii')
-#             if line.startswith('$GPGGA') or line.startswith('$GPRMC'):
-#                 yield line.strip()
-
-# def main():
-#     port = 'COM7'  # Change this to the appropriate port on your system
-#     baudrate = 57600
-
-#     for nmea_message in read_nmea_messages(port, baudrate):
-#         print(nmea_message)
-
-# if __name__ == '__main__':
-#     main()
-
-
-
-
 import serial
 
-# Set the USB port and baud rate
 port = 'COM7'
 baud_rate = 57600
 
-# Open the serial connection
 ser = serial.Serial(port, baud_rate)
 
-# Continuously read and print data from the USB port
 while True:
-    data = ser.readline()
+    data = ser.readline().decode('ascii', errors='replace')
     print(data)
