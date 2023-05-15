c753431a411e63be0d500677a6fa54b59fbadadd
TotoB12 <totobeliard@outlook.com>
2023-05-15 14:10:37 -0700

    .

--- emlid_read_pc/emlid_read_pc.py
+++ emlid_read_pc/emlid_read_pc.py
@@ -1,35 +1,35 @@
-import serial
-
-def read_nmea_messages(port, baudrate):
-    with serial.Serial(port, baudrate, timeout=1) as ser:
-        while True:
-            line = ser.readline().decode('ascii')
-            if line.startswith('$GPGGA') or line.startswith('$GPRMC'):
-                yield line.strip()
-
-def main():
-    port = '/dev/ttyACM0'  # Change this to the appropriate port on your system
-    baudrate = 9600
-
-    for nmea_message in read_nmea_messages(port, baudrate):
-        print(nmea_message)
-
-if __name__ == '__main__':
-    main()
-
-
-
-
 # import serial
 
-# # Set the USB port and baud rate
-# port = '/dev/ttyUSB0'
-# baud_rate = 57600
+# def read_nmea_messages(port, baudrate):
+#     with serial.Serial(port, baudrate, timeout=1) as ser:
+#         while True:
+#             line = ser.readline().decode('ascii')
+#             if line.startswith('$GPGGA') or line.startswith('$GPRMC'):
+#                 yield line.strip()
 
-# # Open the serial connection
-# ser = serial.Serial(port, baud_rate)
+# def main():
+#     port = 'COM7'  # Change this to the appropriate port on your system
+#     baudrate = 57600
 
-# # Continuously read and print data from the USB port
-# while True:
-#     data = ser.readline()
-#     print(data)
+#     for nmea_message in read_nmea_messages(port, baudrate):
+#         print(nmea_message)
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+
+import serial
+
+# Set the USB port and baud rate
+port = 'COM7'
+baud_rate = 57600
+
+# Open the serial connection
+ser = serial.Serial(port, baud_rate)
+
+# Continuously read and print data from the USB port
+while True:
+    data = ser.readline()
+    print(data)
