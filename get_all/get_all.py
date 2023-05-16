dc7fcb16c8efa0ae41011bcb4b25aace5341e776
TotoB12 <totobeliard@outlook.com>
2023-05-16 09:06:55 -0700

    .

--- get_all/get_all.py
+++ get_all/get_all.py
@@ -1,16 +1,3 @@
-# import serial
-
-# port = 'COM7'
-# baud_rate = 57600
-
-# ser = serial.Serial(port, baud_rate)
-
-# while True:
-#     data = ser.readline().decode('ascii', errors='replace')
-#     print(data)
-
-
-
 import serial
 
 def parse_nmea_data(data):
@@ -21,7 +8,7 @@ def parse_nmea_data(data):
         time_utc = data[1][:2] + ":" + data[1][2:4] + ":" + data[1][4:]
         lat = data[2][:2] + "°" + data[2][2:] + "'" + data[3]
         lon = data[4][:3] + "°" + data[4][3:] + "'" + data[5]
-        return f"[GNGGA] Time: {time_utc}, Lat: {lat}, Lon: {lon}"
+        return f"Time: {time_utc}, Lat: {lat}, Lon: {lon}"
 
 emlid = serial.Serial('COM7', 57600, timeout=.1)
 arduino = serial.Serial('COM1', 115200, timeout=.1)
