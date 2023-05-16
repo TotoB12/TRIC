226448bb20553ceb690adbacf2e8ef1186fe5321
TotoB12 <totobeliard@outlook.com>
2023-05-16 10:04:45 -0700

    akd

--- emlid_read_pc/emlid_read_pc.py
+++ emlid_read_pc/emlid_read_pc.py
@@ -23,9 +23,7 @@ def parse_nmea_data(data):
         lon = data[4][:3] + "°" + data[4][3:] + "'" + data[5]
         fix_quality = data[6]
         num_satellites = data[7]
-        hdop = data[8]
         altitude = data[9] + "m"
-        geoidal_sep = data[11] + "m"
         return f"[Plot] Time: {time_utc}, Lat: {lat}, Lon: {lon}, Fix Quality: {fix_quality}, Satellites: {num_satellites}, Altitude: {altitude}"
 
 emlid = serial.Serial('COM7', 57600)
