a9f82108d85f1754e3e9e1aaf6f0fbc50ec3368c
TotoB12 <totobeliard@outlook.com>
2023-05-16 13:41:09 -0700

    yay

--- all_plot/all_plot.py
+++ all_plot/all_plot.py
@@ -1,6 +1,7 @@
 import serial
-import matplotlib.pyplot as plt
 import utm
+import plotly.graph_objs as go
+import plotly
 
 def parse_nmea_data(data):
     data = data.strip().split(',')
@@ -21,20 +22,25 @@ def parse_nmea_data(data):
 
         return time_utc, lat, lon
 
-emlid = serial.Serial('COM7', 57600)
-
-plt.ion()
-fig, ax = plt.subplots()
+emlid = serial.Serial('COM7', 57600, timeout=.1)
+arduino = serial.Serial('COM9', 9600, timeout=.1)
 
 origin_set = False
 origin_x, origin_y = 0, 0
+
+x_data = []
+y_data = []
+z_data = []
 
 while True:
     data = emlid.readline().decode('ascii', errors='replace')
     parsed_data = parse_nmea_data(data)
     if parsed_data:
+        distance = arduino.readline()[:-1].decode('ascii', errors='replace')
+        if distance:
+            d = float(distance)
         time_utc, lat, lon = parsed_data
-        print(f"[Plot] Time: {time_utc}, Lat: {lat}, Lon: {lon}")
+        print(f"[Rover] Time: {time_utc}, Lat: {lat}, Lon: {lon}, Dist: {d} cm")
 
         x, y, _, _ = utm.from_latlon(lat, lon)
 
@@ -45,6 +51,12 @@ while True:
         rel_x = x - origin_x
         rel_y = y - origin_y
 
-        ax.scatter(rel_x, rel_y, c='green', marker='o')
-        plt.draw()
-        plt.pause(0.01)
+        x_data.append(rel_x)
+        y_data.append(rel_y)
+        z_data.append(d)
+
+        trace = go.Scatter3d(x=x_data, y=y_data, z=z_data, mode='markers', marker=dict(size=5, color=z_data, colorscale='Viridis', opacity=0.8))
+        data = [trace]
+        layout = go.Layout(scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Distance (cm)'), width=700, margin=dict(l=0, r=0, b=0, t=0))
+        fig = go.Figure(data=data, layout=layout)
+        plotly.offline.plot(fig, filename='map.html', auto_open=False)
