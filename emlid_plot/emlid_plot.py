56471aa98af1814b1463b9d2f69a218611e1f46b
TotoB12 <totobeliard@outlook.com>
2023-05-16 10:51:27 -0700

    jjj

--- emlid_plot/emlid_plot.py
+++ emlid_plot/emlid_plot.py
@@ -1,12 +1,6 @@
 import serial
 import matplotlib.pyplot as plt
-
-def dms_to_decimal(dms, direction):
-    degrees, minutes = dms[:2], dms[2:]
-    decimal = float(degrees) + float(minutes) / 60
-    if direction in ['S', 'W']:
-        decimal = -decimal
-    return decimal
+import utm
 
 def parse_nmea_data(data):
     data = data.strip().split(',')
@@ -14,25 +8,43 @@ def parse_nmea_data(data):
 
     if data_type == "GNGGA":
         time_utc = data[1][:2] + ":" + data[1][2:4] + ":" + data[1][4:]
-        lat = dms_to_decimal(data[2], data[3])
-        lon = dms_to_decimal(data[4], data[5])
+        try:
+            lat = float(data[2][:2]) + float(data[2][2:]) / 60
+            if data[3] == 'S':
+                lat = -lat
+            lon = float(data[4][:3]) + float(data[4][3:]) / 60
+            if data[5] == 'W':
+                lon = -lon
+        except ValueError:
+            print("bad data")
+            return None
+
         return time_utc, lat, lon
 
-plt.ion()
-origin_set = False
-origin_lat = 0
-origin_lon = 0
-
 emlid = serial.Serial('COM7', 57600)
+
+plt.ion()
+fig, ax = plt.subplots()
+
+origin_set = False
+origin_x, origin_y = 0, 0
 
 while True:
     data = emlid.readline().decode('ascii', errors='replace')
     parsed_data = parse_nmea_data(data)
     if parsed_data:
         time_utc, lat, lon = parsed_data
+        print(f"[Plot] Time: {time_utc}, Lat: {lat}, Lon: {lon}")
+
+        x, y, _, _ = utm.from_latlon(lat, lon)
+
         if not origin_set:
-            origin_lat = lat
-            origin_lon = lon
+            origin_x, origin_y = x, y
             origin_set = True
-        plt.scatter(lon - origin_lon, lat - origin_lat, color='green', marker='o')
-        plt.pause(0.1)
+
+        rel_x = x - origin_x
+        rel_y = y - origin_y
+
+        ax.scatter(rel_x, rel_y, c='green', marker='o')
+        plt.draw()
+        plt.pause(0.01)
