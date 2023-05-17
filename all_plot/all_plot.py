a37ae7704cf52c00e81388ad58e04165b0b91d0f
TotoB12 <totobeliard@outlook.com>
2023-05-17 15:57:12 -0700

    ..

--- all_plot/all_plot.py
+++ all_plot/all_plot.py
@@ -2,10 +2,11 @@ import serial
 import utm
 import plotly.graph_objs as go
 import plotly
-import numpy as np
 import os
 import datetime
 import time
+import keyboard
+import numpy as np
 
 def parse_nmea_data(data):
     data = data.strip().split(',')
@@ -25,10 +26,6 @@ def parse_nmea_data(data):
             return None
 
         return time_utc, lat, lon
-    
-def moving_average(data, window_size):
-    window = np.ones(int(window_size))/float(window_size)
-    return np.convolve(data, window, 'valid')
 
 emlid = serial.Serial('COM7', 11520, timeout=.1)
 arduino = serial.Serial('COM9', 9600, timeout=.1)
@@ -36,11 +33,10 @@ arduino = serial.Serial('COM9', 9600, timeout=.1)
 origin_set = False
 origin_x, origin_y = 0, 0
 
-x_data = []
-y_data = []
-z_data = []
-marker_size = []
-marker_color = []
+x_data = np.array([])
+y_data = np.array([])
+z_data = np.array([])
+marker_color = np.array([])
 
 time_data = []
 
@@ -53,18 +49,12 @@ time.sleep(0.1)
 
 with open(os.path.join('data', folder_name, 'data.txt'), 'w') as data_file:
     while True:
-        # if arduino.in_waiting > 0:
-        #     distance = arduino.readline()[:-1].decode('ascii', errors='replace')
-        #     if int(distance) < 200:
-        #         d = float(distance)
         if emlid.in_waiting > 0:
             data = emlid.readline().decode('ascii', errors='replace')
             parsed_data = parse_nmea_data(data)
             if parsed_data:
                 if arduino.in_waiting > 0:
                     distance = arduino.readline()[:-1].decode('ascii', errors='replace')
-                    # if int(distance) < 200:
-                    #     d = float(distance)
                     d = float(distance)
                 time_utc, lat, lon = parsed_data
                 print(f"[Rover] Time: {time_utc}, Lat: {lat}, Lon: {lon}, Dist: {d} cm")
@@ -78,27 +68,27 @@ with open(os.path.join('data', folder_name, 'data.txt'), 'w') as data_file:
                 rel_x = x - origin_x
                 rel_y = y - origin_y
 
-                x_data.append(rel_x)
-                y_data.append(rel_y)
-                z_data.append(d)
-                marker_color.append(d)
+                x_data = np.append(x_data, rel_x)
+                y_data = np.append(y_data, rel_y)
+                z_data = np.append(z_data, d)
+                marker_color = np.append(marker_color, d)
 
                 time_data.append(time.time())
 
                 data_file.write(f"{time_utc}, {rel_x}, {rel_y}, {d}\n")
-
-        if len(x_data) >= 5:
-
-            smoothed_z_data = moving_average(z_data, 5)
-
-            trace3d = go.Scatter3d(x=x_data, y=y_data, z=smoothed_z_data, mode='lines+markers', marker=dict(size=5, color=marker_color, colorscale='Viridis', opacity=0.8), line=dict(color='darkblue', width=2))
+                data_file.flush()
+                
+        if keyboard.is_pressed('s') :
+            trace3d = go.Scatter3d(x=x_data, y=y_data, z=z_data, mode='lines+markers', marker=dict(size=5, color=marker_color, colorscale='Viridis', opacity=0.8), line=dict(color='darkblue', width=2))
             data3d = [trace3d]
             layout3d = go.Layout(scene=dict(xaxis_title='Distance X (m)', yaxis_title='Distance Y (m)', zaxis_title='Distance Z (cm)'), margin=dict(l=0, r=0, b=0, t=0))
             fig3d = go.Figure(data=data3d, layout=layout3d)
             plotly.offline.plot(fig3d, filename=os.path.join('data', folder_name, 'map.html'), auto_open=False)
 
-            trace2d = go.Scatter(x=time_data, y=smoothed_z_data, mode='lines+markers', marker=dict(size=5, color=marker_color, colorscale='Viridis', opacity=0.8), line=dict(color='darkblue', width=2))
+            trace2d = go.Scatter(x=time_data, y=z_data, mode='lines+markers', marker=dict(size=5, color=marker_color, colorscale='Viridis', opacity=0.8), line=dict(color='darkblue', width=2))
             data2d = [trace2d]
             layout2d = go.Layout(xaxis_title='Time (s)', yaxis_title='Distance (cm)', margin=dict(l=0, r=0, b=0, t=0))
             fig2d = go.Figure(data=data2d, layout=layout2d)
             plotly.offline.plot(fig2d, filename=os.path.join('data', folder_name, 'graph.html'), auto_open=False)
+
+            break
