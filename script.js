c8aa644b98c906a13fc1315396c23067b9e021bf
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-25 16:44:31 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('viewRawFiles').addEventListener('click', function() {
+});
+
+document.getElementById('viewScripts').addEventListener('click', function() {
+});
+
+document.getElementById('viewDocumentation').addEventListener('click', function() {
+});
+
+document.getElementById('viewData').addEventListener('click', function() {
+});
+
+document.getElementById('compareData').addEventListener('click', function() {
+});
+
+document.getElementById('replayData').addEventListener('click', function() {
+});
