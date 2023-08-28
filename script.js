5adf22c704b687a1cbfd6c0efcfd8f3ce615c1a5
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-28 10:41:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const openPopupButtons = document.querySelectorAll('.open-popup');
 const closePopupButtons = document.querySelectorAll('.close-popup');
 
+fetch('https://raw.githubusercontent.com/TotoB12/TRIC/main/README.md')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('main-doc-content').innerHTML = data;
+  });
+
 openPopupButtons.forEach(button => {
     button.addEventListener('click', () => {
         const popupId = button.getAttribute('data-popup');
